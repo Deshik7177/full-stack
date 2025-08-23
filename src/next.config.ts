06b72322bea2a48b1031c3975b254291da510fc7
Deshik Paila <pdhanadeshik7177@gmail.com>
2025-08-23 08:39:14 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.ts
+++ src/next.config.ts
@@ -19,22 +19,20 @@ const baseConfig: NextConfig = {
   },
 };
 
-const devConfig: NextConfig = {
-  ...baseConfig,
-  // START: DEV CONFIG
-  // The following properties are set for development only.
-  // They will be removed in production builds.
-  experimental: {
-    ...baseConfig.experimental,
-    // This is needed to allow the Next.js dev server to be accessed from the Firebase Studio preview.
-    allowedDevOrigins: [
-      '*.cloudworkstations.dev',
-      '*.firebase.studio',
-    ],
-  },
-  // END: DEV CONFIG
-};
+let nextConfig: NextConfig = baseConfig;
 
-const nextConfig = process.env.NODE_ENV === 'development' ? devConfig : baseConfig;
+if (process.env.NODE_ENV === 'development') {
+  nextConfig = {
+    ...baseConfig,
+    experimental: {
+      ...baseConfig.experimental,
+      // This is needed to allow the Next.js dev server to be accessed from the Firebase Studio preview.
+      allowedDevOrigins: [
+        '*.cloudworkstations.dev',
+        '*.firebase.studio',
+      ],
+    },
+  };
+}
 
 export default nextConfig;
